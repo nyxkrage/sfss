371302a816e58cc97b46d4e409cbbad59cd45ac9
Carsten Kragelund <carsten@kragelund.dev>
2021-03-10 09:54:29 +0100

    Added CORS header to files

--- src/sfss_format/sfss_format.rs
+++ src/sfss_format/sfss_format.rs
@@ -349,6 +349,7 @@ impl<'r> Responder<'r, 'static> for SfssFile {
         resp
             .header(self.content_type())
             .header(Header::new("Cache-Control", "max-age=31536000"))
+            .header(Header::new("Access-Control-Allow-Origin", "*"))
             .header(Header::new(
                 "Content-Disposition",
                 format!(
